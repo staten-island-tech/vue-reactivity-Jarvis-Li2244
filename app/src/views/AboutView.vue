b477c1cf457157b99fe8665ab40cb94c89888faf
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2025-01-16 14:02:38 -0500

    dsafsfasfwsydxytgjgk,h.

---
+++ app/src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <SubmitForm/>  
+  </div>
+</template>
+
+<script setup>
+import SubmitForm from '@/components/SubmitForm.vue';
+</script>
+
+<style lang="scss" scoped>
+
+</style>
